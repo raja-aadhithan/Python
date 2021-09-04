4cfbeaac6eece31d20eeec5113cf5f6c03413b55
Raja Aadhithan <raja.aadhithan.t@gmail.com>
2021-09-04 19:22:43 +0530

    ini

---
+++ string.py
@@ -0,0 +1,19 @@
+# Python String Operations
+str1 = 'Hello'
+str2 ='World!'
+
+# using +
+print('str1 + str2 = ', str1 + str2)
+
+# using *
+print('str1 * 3 =', str1 * 3)
+
+#slicing string
+txt = "Raja Aadhithan"
+
+x = txt.split()
+
+print('first name is : ' ,x[0])
+print('last name is : ' ,x[1])
+
+
